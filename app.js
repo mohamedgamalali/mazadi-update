ca3b3e5bd33030f614cf66be3d5ab6dae28e3db6
mohamedgamalali <mg7404809@gmail.com>
2020-07-02 00:13:06 +0200

    first commit

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use('/vid',express.static(path.join(__dirname,'vid')));
   
 //headers meddlewere
 app.use((req,res,next)=>{
-    //res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Headers','Content-type,Authorization');
     next();
